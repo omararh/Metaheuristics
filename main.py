e42d9983b185336e77c1658f8828a7da1c7bffbf
omararh <omararharbi77@gmail.com>
2024-11-05 04:12:32 +0100

    add SimulatedAnnealing implementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.GraphParserService import GraphParser
+from src.SimulatedAnnealingSearch import SimulatedAnnealing
 from src.SolutionService import Solution
 from src.SolutionService import Validator
 from src.TabuSearch import TabuSearch
@@ -13,8 +14,21 @@ if __name__ == "__main__":
     G = GraphParser.parseFile(file_path)
     G = Solution.generateInitialSolution(G)
     print("result for initial solution ---> ", Validator.objectiveFunction(G))
-    # to try : 10 000 it, 50 v
+    '''
+    # to try : 10 000 it, 50 
     tabuSearch = TabuSearch(G, 1000, 100)
     tabuSearch.search()
     bestSolution = tabuSearch.getBestSolution()
+    print("result for best solution ---> ", Validator.objectiveFunction(bestSolution))
+    '''
+
+    T0 = 100  # Température initiale
+    Tf = 0.01  # Température finale
+    alpha = 0.95  # Facteur de refroidissement
+    nbMaxIt = 10000000  # Nombre maximal d'itérations
+    nbNeighbors = 500  # Nombre de voisins par itération
+
+    simulatedAnnealing = SimulatedAnnealing(G, T0, Tf, alpha, nbMaxIt, nbNeighbors)
+    simulatedAnnealing.search()
+    bestSolution = simulatedAnnealing.getBestSolution()
     print("result for best solution ---> ", Validator.objectiveFunction(bestSolution))
---
+++ src/SimulatedAnnealingSearch.py
@@ -0,0 +1,59 @@
+from src.LocalSearch import LocalSearch
+from src.ValidatorService import Validator
+import math
+import random
+
+
+class SimulatedAnnealing(LocalSearch):
+
+    def __init__(self, G, T0, Tf, alpha, nbMaxIt, nbNeighbors):
+        super().__init__(G, nbMaxIt, nbNeighbors)
+        self.T = T0
+        self.Tf = Tf
+        self.alpha = alpha
+        self.currentTemperature = T0
+
+    @staticmethod
+    def acceptanceProbability(delta_f, T):
+        """
+        Compute the acceptance probability based on the Metropolis criterion.
+        :param delta_f: Change in objective function value
+        :param T: Current temperature
+        :return: Probability of accepting worse solution
+        """
+        if delta_f < 0:
+            return 1
+        return math.exp(-delta_f / T)
+
+    def coolDown(self):
+        """
+        Update the current temperature using the cooling factor.
+        """
+        self.currentTemperature *= self.alpha
+
+    def search(self):
+        """
+        Simulated annealing implementation
+        :return:
+        """
+        self.setInitialSolution()
+        cp = 0
+        while self.currentTemperature > self.Tf and cp < self.nbMaxIt:
+            self.generateNeighborhood()
+            savedCurrentSolution = self.currentSolution
+            self.updateCurrentSolution()
+
+            objectiveSolution = Validator.objectiveFunction(savedCurrentSolution)
+            objectiveUpdatedSolution = Validator.objectiveFunction(self.currentSolution)
+            delta_f = objectiveUpdatedSolution - objectiveSolution
+
+            objectiveBestSolution = Validator.objectiveFunction(self.bestSolution)
+            if random.random() < SimulatedAnnealing.acceptanceProbability(delta_f, self.currentTemperature):
+                if objectiveBestSolution > objectiveUpdatedSolution:
+                    self.bestSolution = self.currentSolution
+            else:
+                self.currentSolution = savedCurrentSolution
+
+            self.cleanNeighborhood()
+            self.coolDown()
+            cp += 1
